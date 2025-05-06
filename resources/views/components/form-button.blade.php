794656931cb8eff45b8940044fa7288b790e8728
Matheus <matheusmartinscia@gmail.com>
2025-05-06 20:18:47 -0300

    Criei um componente para botão de sumbmit

---
+++ resources/views/components/form-button.blade.php
@@ -0,0 +1,3 @@
+<button type="submit" class="bg-blue-400 text-white text-2xl font-semibold text-shadow-white px-5 py-2 rounded cursor-pointer hover:bg-blue-500 hover:text-shadow-2xs transition-all ease-in-out duration-100">
+    {{ $text }}
+</button>
