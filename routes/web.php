58a911f8371dba097b1a22e87c02d6ba86175803
Matheus <matheusmartinscia@gmail.com>
2025-05-08 19:56:49 -0300

    Correção v2

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth")->group(function(){
-    Route::get('/', [MainController::class, 'home'])->name('home');
-    Route::view('/about', 'about')->name('about');
+    
 });
+
+Route::get('/', [MainController::class, 'home'])->name('home');
+Route::view('/about', 'about')->name('about');
 
     
