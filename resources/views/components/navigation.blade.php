d587cdbd81ca90818d5ad2d8ddde5b0e6e1fd1b7
Matheus <matheusmartinscia@gmail.com>
2025-05-08 19:58:44 -0300

    Feat: Validação de autenticação para acessar página da conta

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -5,11 +5,13 @@
         <li class="text-white text-3xl font-medium my-6 hover:text-blue-900 transition-all ease-in-out duration-100"><a href="{{ route('home') }}">Produtos</a></li>
     @endif
 
-    @if($currentPage == "account")
-        <li class="text-blue-900 text-3xl font-medium border-b-3 border-blue-800 py-6"><a href="#">Conta</a></li>
-    @else 
-        <li class="text-white text-3xl font-semibold my-6 hover:text-blue-900 transition-all ease-in-out duration-100"><a href="#">Conta</a></li>
-    @endif
+    @auth
+        @if($currentPage == "account")
+            <li class="text-blue-900 text-3xl font-medium border-b-3 border-blue-800 py-6"><a href="#">Conta</a></li>
+        @else 
+            <li class="text-white text-3xl font-semibold my-6 hover:text-blue-900 transition-all ease-in-out duration-100"><a href="#">Conta</a></li>
+        @endif
+    @endauth
 
     @if($currentPage == "about")
         <li class="text-blue-900 text-3xl font-medium border-b-3 border-blue-800 py-6"><a href="#">Sobre</a></li>
