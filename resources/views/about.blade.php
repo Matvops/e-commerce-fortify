015a0c4de4f44366c38b73d9a1b2d998dba292e3
Matheus <matheusmartinscia@gmail.com>
2025-05-12 18:29:37 -0300

    Feat: Adicionei opção de fazer login na tela 'Sobre', caso o usuário não esteja logado

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -21,6 +21,16 @@
                     </button>    
                 </form>   
             @endauth
+
+            @guest
+                <div class="flex gap-4 mb-2">
+                    <a href="{{ route('login') }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strok-width="0.5" stroke="black" class="size-16 hover:fill-blue-200 transition-all ease-in-out duration-100">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
+                        </svg>
+                    </a>
+                </div>
+            @endguest
         </header>
 
         <main class="w-[60%] mx-auto mt-36">
