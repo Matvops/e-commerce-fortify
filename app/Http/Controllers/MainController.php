c309f19270b06fbee4d66bb5f57be3134be2ff4d
Matheus <matheusmartinscia@gmail.com>
2025-05-12 18:28:25 -0300

    Feat: Criei método controlador para consultador dados dos produtos e retornar view na view home

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\MainService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
 {
-    public function home(){
-        
-        return view('home');
+    private $service;
+
+    public function __construct()
+    {
+        $this->service = new MainService;
     }
+    
+    public function home(){
+        $products = $this->service->getAllProducts();
+        $topSeller = $this->service->getTopsSeller();
+        error_log(json_encode($topSeller));
+        return view('home', ['products' => $products, 'highlights' => $topSeller]);
+    }
+
+    
 }
