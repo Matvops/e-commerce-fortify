eb4c9fbd1fb0f458ae16c30f09544bb862d611cd
Matheus <matheusmartinscia@gmail.com>
2025-05-13 19:11:36 -0300

    Feat: Adiciona rota para página de conta

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth")->group(function(){
-    
+    Route::view('/account', 'account')->name('account');
 });
 
 Route::get('/', [MainController::class, 'home'])->name('home');
