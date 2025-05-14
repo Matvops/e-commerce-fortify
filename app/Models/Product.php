3bbedf37f1b53aeadcdbe83b61eceb9f78f2f8ee
Matheus <matheusmartinscia@gmail.com>
2025-05-13 21:01:28 -0300

    Feat: Cria relação muitos-para-muitos com a tabela carts

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
     protected $database = "products";
     protected $primaryKey = "product_id";
+
+    public function carts(): BelongsToMany
+    {
+        return $this->belongsToMany(Cart::class, "product_cart", "pc_product_id", "pc_product_id");
+    }
 }
