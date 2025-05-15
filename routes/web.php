8322556cd61cf9a2683c0711e2f0cc78f7ea5823
Matheus <matheusmartinscia@gmail.com>
2025-05-14 21:39:00 -0300

    Feat: Cria rota para remover produto do carrinho

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware("auth")->group(function(){
     Route::view('/account', 'account')->name('account');
     Route::get('/cart', [MainController::class, "getCart"])->name("cart");
+    Route::delete('/deleteProductCart', [MainController::class, 'removeProductCart'])->name('product-cart.remove');
 });
 
 Route::get('/', [MainController::class, 'home'])->name('home');
