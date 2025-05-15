7185afba8e82bcdf2e88b95cf693985e749259e0
Matheus <matheusmartinscia@gmail.com>
2025-05-14 21:44:27 -0300

    Feat: Cria estrutura para enviar requisição delete e retornar o resultado pelo toast

--- resources/views/components/product-cart-card.blade.php
+++ resources/views/components/product-cart-card.blade.php
@@ -10,12 +10,21 @@
     </div>
 
     <div class="pr-4 self-center">
-        <form action="" method="POST">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1" stroke="white" class="size-24 fill-blue-900 cursor-pointer hover:fill-blue-400 transition-all duration-100 ease-in-out">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-            </svg>
-        </form>
-            
-        
+        <form action="{{ route('product-cart.remove') }}" method="POST">
+            @csrf
+            @method("DELETE")
+
+            <input type="hidden" name="product_id" value="{{ Crypt::encrypt($product->product_id) }}">
+
+            <button type="submit">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="1" stroke="white" class="size-24 fill-blue-900 cursor-pointer hover:fill-blue-400 transition-all duration-100 ease-in-out">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                </svg>
+            </button>
+
+            @if(session('productRemoveMessage'))
+                <x-toast :status-="session('productRemoveStatus')" :message="session('productRemoveMessage')" />
+            @endif
+        </form> 
     </div>
 <li>
