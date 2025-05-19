7007fed35b4531204991c230ca9abaff6a20733d
Matheus <matheusmartinscia@gmail.com>
2025-05-19 17:12:53 -0300

    Feat: Cria rota para método controlador addProductOnCart

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::middleware("auth")->group(function(){
     Route::view('/account', 'account')->name('account');
     Route::get('/cart', [MainController::class, "getCart"])->name("cart");
     Route::delete('/deleteProductCart', [MainController::class, 'removeProductCart'])->name('product-cart.remove');
+    Route::post('/addProductOnCart', [MainController::class, 'addProductOnCart'])->name('product-cart.add');
 });
 
 Route::get('/', [MainController::class, 'home'])->name('home');
