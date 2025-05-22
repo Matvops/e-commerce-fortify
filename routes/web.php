e5e285e17783a0460d165a1033b3b581b1fd5e65
Matheus <matheusmartinscia@gmail.com>
2025-05-22 19:45:59 -0300

    Feat: Cria rota para método controlador

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth")->group(function(){
-    Route::view('/account', 'account')->name('account');
+    Route::get('/account', [MainController::class, 'getOrders'])->name('account');
     Route::get('/cart', [MainController::class, "getCart"])->name("cart");
     Route::delete('/deleteProductCart', [MainController::class, 'removeProductCart'])->name('product-cart.remove');
     Route::post('/addProductOnCart', [MainController::class, 'addProductOnCart'])->name('product-cart.add');
