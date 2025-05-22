b99bc01570619c7cafef6873eb4f99139e0cc9fa
Matheus <matheusmartinscia@gmail.com>
2025-05-22 20:04:57 -0300

    Feat: Altera cor do header

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,6 +1,6 @@
 <x-layouts.main_layout title="account">
     <x-slot:content>
-        <header class="flex items-end bg-blue-100 px-4 mt-[1px]">
+        <header class="flex items-end bg-blue-500/30 px-4 mt-[1px]">
             <div class="m-0 w-fit py-4">
                 <a href="{{ route('home') }}">
                     <img src="{{ asset('logo.svg') }}" alt="logo" class="w-20 max-w-35" />
