616704cfc005bdc6355ba19623d451686a3bb4d5
Matheus <matheusmartinscia@gmail.com>
2025-05-25 11:43:25 -0300

    Feat: Altera rotas para novo CartController

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,15 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth")->group(function(){
-    Route::get('/account', [MainController::class, 'getOrders'])->name('account');
-    Route::get('/cart', [MainController::class, "getCart"])->name("cart");
-    Route::delete('/deleteProductCart', [MainController::class, 'removeProductCart'])->name('product-cart.remove');
-    Route::post('/addProductOnCart', [MainController::class, 'addProductOnCart'])->name('product-cart.add');
-    Route::post('/makeOrder', [MainController::class, 'makeOrder'])->name('order.make');
+    Route::get('/account', [CartController::class, 'getOrders'])->name('account');
+    Route::get('/cart', [CartController::class, "getCart"])->name("cart");
+    Route::delete('/deleteProductCart', [CartController::class, 'removeProductCart'])->name('product-cart.remove');
+    Route::post('/addProductOnCart', [CartController::class, 'addProductOnCart'])->name('product-cart.add');
+    Route::post('/makeOrder', [CartController::class, 'makeOrder'])->name('order.make');
 });
 
 Route::get('/', [MainController::class, 'home'])->name('home');
