6300ce8f60557b03c3ef53de482fb7d7b0d559c8
Matheus <matheusmartinscia@gmail.com>
2025-05-25 14:32:02 -0300

    Feat: Altera controller chamado em rotas que manipulam/consultam orders

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,15 @@
 
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware("auth")->group(function(){
-    Route::get('/account', [CartController::class, 'getOrders'])->name('account');
+    Route::get('/account', [OrderController::class, 'getOrders'])->name('account');
     Route::get('/cart', [CartController::class, "getCart"])->name("cart");
     Route::delete('/deleteProductCart', [CartController::class, 'removeProductCart'])->name('product-cart.remove');
     Route::post('/addProductOnCart', [CartController::class, 'addProductOnCart'])->name('product-cart.add');
-    Route::post('/makeOrder', [CartController::class, 'makeOrder'])->name('order.make');
+    Route::post('/makeOrder', [OrderController::class, 'makeOrder'])->name('order.make');
 });
 
 Route::get('/', [MainController::class, 'home'])->name('home');
