b6b37c3d641db85e617e1bde43d3f145d74ae8b5
Matheus <matheusmartinscia@gmail.com>
2025-05-25 15:08:05 -0300

    Fix: Corrige tratamento de erro

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,15 +3,17 @@
 namespace App\Services;
 
 use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\Translation\Exception\NotFoundResourceException;
 
 class UserService {
 
     public static function getUser() {
-        return User::where('id', Auth::id())
-            ->firstOrFail(function() {
-                throw new NotFoundResourceException('Usuário não encontrado.');
-            });
+        try {
+            return User::where('id', Auth::id())->firstOrFail();
+        } catch(ModelNotFoundException ) {
+            throw new NotFoundResourceException('Usuário não encontrado');
+        }
     }
 }
